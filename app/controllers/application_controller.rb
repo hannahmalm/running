68dd8c044fd97025365b0c38ee170f44ec90d41b
Hannah Malm <hannahemalm616@gmail.com>
2021-10-01 17:38:58 -0400

    added in helpers to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,5 +24,43 @@ class ApplicationController < Sinatra::Base
             redirect '/login'
         end 
     end 
-    
+
+    get '/account' do 
+        @user = User.find(session[:user_id])
+        erb :account 
+    end 
+
+    get "/login" do 
+        erb :login 
+    end 
+
+    post "/login" do 
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect to "/account"
+        else 
+            redirect to "/failure"
+        end 
+    end 
+
+    get "/failure" do 
+        erb :failure 
+    end 
+
+    get "/logout" do 
+        session.clear 
+        redirect "/"
+    end 
+
+    helpers do 
+        def logged_in?
+            !!session[:user_id]
+        end 
+
+        def current_user 
+            User.find(session[:user_id])
+        end 
+    end 
+
 end 
