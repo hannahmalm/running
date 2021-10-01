8dbaf5eeae5c7e864dad110eccfd0847cd723c6a
Hannah Malm <hannahemalm616@gmail.com>
2021-10-01 17:34:36 -0400

    added signup logic in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,18 @@ class ApplicationController < Sinatra::Base
     get "/" do 
         erb :index 
     end 
+
+    get "/signup" do 
+        erb :signup
+    end 
+
+    post "/signup" do 
+        if params[:username] == "" || params[:password] == ""
+            redirec '/failure'
+        else 
+            User.create(username: params[:username], password: params[:password])
+            redirect '/login'
+        end 
+    end 
     
 end 
