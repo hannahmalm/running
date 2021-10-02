ebbb16c94b91124d74ac2a38c86784479775a184
Hannah Malm <hannahemalm616@gmail.com>
2021-10-02 10:39:09 -0400

    create edit log logic

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -15,10 +15,33 @@ class LogController < Sinatra::Base
     end 
 
     post '/logs' do 
-        @logs = Log.create(:date => params[:date], :distance => params[:distance], :pace => params[:pace], :avg_heart_rate => params[:avg_heart_rate], :notes => params[:notes], :owner_id => params[:owner_id])
+        @log = Log.create(:date => params[:date], :distance => params[:distance], :pace => params[:pace], :avg_heart_rate => params[:avg_heart_rate], :notes => params[:notes], :owner_id => params[:owner_id])
         redirect to "/logs/#{@log.id}"
     end 
+    
+    #show log 
+    get '/logs/:id' do 
+        @log = Log.find_by_id(params[:id])
+        erb :show 
+    end 
 
+    #edit log 
+    get '/logs/:id/edit' do 
+        @log = Log.find_by_id(params[:id])
+        erb :edit 
+    end 
+
+    patch '/logs/:id' do 
+        @log = Log.find_by_id(params[:id])
+        @log.date = params[:date]
+        @log.distance = params[:distance]
+        @log.pace = params[:pace]
+        @log.avg_heart_rate = params[:avg_heart_rate]
+        @log.notes = params[:notes]
+        @log.owner_id = params[:owner_id]
+        @log.save 
+        redirect to "/log/#{@log.id}"
+    end 
         
 
 end 
