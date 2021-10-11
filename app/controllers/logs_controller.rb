d465eaf64c2c6b2cf337ddd121d719db10541ad2
Hannah Malm <hannahemalm616@gmail.com>
2021-10-11 17:45:11 -0400

    deleted out unused helpers

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -98,15 +98,4 @@ class LogsController < ApplicationController
         end
     end 
 
-    helpers do 
-        def logged_in?
-            current_user
-        end 
-
-        def current_user 
-            @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
-        end 
-    end 
-        
-
 end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,14 +43,4 @@ class UsersController < ApplicationController
         end 
     end
 
-
-    helpers do 
-        def logged_in?
-            !!current_user
-        end 
-
-        def current_user 
-            @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-        end 
-    end 
 end 
