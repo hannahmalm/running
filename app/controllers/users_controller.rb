7d7358bfadf328102b95cde60226ca4392651290
Hannah Malm <hannahemalm616@gmail.com>
2021-11-21 14:20:07 -0500

    updated username requirements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
    end 
 
     post "/signup" do #Once a user inputs into the form, post it
+        #1. A user must input a username AND a password. Both fields must be populated
+        #2. A user must have a unique username. If they dont, they will get an error that the username is taken. 
         if params[:username] == "" || params[:password] == "" #Ensure form is complete
             #redirect '/failure' #else redirect to failture
             flash[:message] = "Username and Password must not be blank"
@@ -16,7 +18,7 @@ class UsersController < ApplicationController
                 session[:user_id] = user.id #create the session with the user id
                 redirect "/logs" #redirect to the homepage
             else
-                flash[:username] = "Sorry, this username is already taken"
+                flash[:username] = "Sorry, this username is already taken. Please choose a different username"
                 redirect "/signup"
             end 
         end 
