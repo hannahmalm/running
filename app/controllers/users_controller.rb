6a6702c19cd2cd6b7a892029ea205b5d2342ac82
Hannah Malm <hannahemalm616@gmail.com>
2021-11-22 19:38:11 -0500

    commented out post signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,25 +4,25 @@ class UsersController < ApplicationController
         erb :'/users/signup'
    end 
 
-    post "/signup" do #Once a user inputs into the form, post it
-        #1. A user must input a username AND a password. Both fields must be populated
-        #2. A user must have a unique username. If they dont, they will get an error that the username is taken. 
-        if params[:username] == "" || params[:password] == "" #Ensure form is complete
-            #redirect '/failure' #else redirect to failture
-            flash[:message] = "Username and Password must not be blank"
-            erb :'/users/signup'
-        else 
-            user = User.create(params)
-            if user.valid?
-                #user = User.create(username: params[:username], password: params[:password]) #If the form is complete, create the user
-                session[:user_id] = user.id #create the session with the user id
-                redirect "/logs" #redirect to the homepage
-            else
-                flash[:username] = "Sorry, this username is already taken. Please choose a different username"
-                redirect "/signup"
-            end 
-        end 
-    end 
+    # post "/signup" do #Once a user inputs into the form, post it
+    #     #1. A user must input a username AND a password. Both fields must be populated
+    #     #2. A user must have a unique username. If they dont, they will get an error that the username is taken. 
+    #     if params[:username] == "" || params[:password] == "" #Ensure form is complete
+    #         #redirect '/failure' #else redirect to failture
+    #         flash[:message] = "Username and Password must not be blank"
+    #         erb :'/users/signup'
+    #     else 
+    #         user = User.create(params)
+    #         if user.valid?
+    #             #user = User.create(username: params[:username], password: params[:password]) #If the form is complete, create the user
+    #             session[:user_id] = user.id #create the session with the user id
+    #             redirect "/logs" #redirect to the homepage
+    #         else
+    #             flash[:username] = "Sorry, this username is already taken. Please choose a different username"
+    #             redirect "/signup"
+    #         end 
+    #     end 
+    # end 
 
         post "/signup" do 
             @user = User.new(params)
