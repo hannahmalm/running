6684644cd423aef7ed0ab6055063454910f1a080
Hannah Malm <hannahemalm616@gmail.com>
2021-11-24 16:02:22 -0500

    redirecting login to logs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,27 +4,7 @@ class UsersController < ApplicationController
         erb :'/users/signup'
    end 
 
-    # post "/signup" do #Once a user inputs into the form, post it
-    #     #1. A user must input a username AND a password. Both fields must be populated
-    #     #2. A user must have a unique username. If they dont, they will get an error that the username is taken. 
-    #     if params[:username] == "" || params[:password] == "" #Ensure form is complete
-    #         #redirect '/failure' #else redirect to failture
-    #         flash[:message] = "Username and Password must not be blank"
-    #         erb :'/users/signup'
-    #     else 
-    #         user = User.create(params)
-    #         if user.valid?
-    #             #user = User.create(username: params[:username], password: params[:password]) #If the form is complete, create the user
-    #             session[:user_id] = user.id #create the session with the user id
-    #             redirect "/logs" #redirect to the homepage
-    #         else
-    #             flash[:username] = "Sorry, this username is already taken. Please choose a different username"
-    #             redirect "/signup"
-    #         end 
-    #     end 
-    # end 
-
-     post "/signup" do #local variables because not rendering ERB - DONE
+    post "/signup" do #local variables because not rendering ERB - DONE
         user = User.new(params) #set local variable and inialize a new user by passing in the params
         if user.save #save the user only if the validations pass --> validations are on user model
             session[:user_id] = user.id #set the session equal to the user.id
@@ -34,9 +14,9 @@ class UsersController < ApplicationController
             flash[:errors] = "Signup Failure: #{user.errors.full_messages.to_sentence}" #https://stackoverflow.com/questions/15043272/errors-full-messages-format-in-rails-3
             redirect "/signup" 
         end 
-     end 
+    end 
 
-    get "/login" do #Get the login form
+    get "/login" do #Get the login form -DONE
          erb :'/users/login' 
     end 
 
@@ -44,7 +24,8 @@ class UsersController < ApplicationController
         user = User.find_by(username: params[:username]) #find_by needs a key value pair
         if user && user.authenticate(params[:password]) #if the username and password match
             session[:user_id] = user.id #create the session - will only work if set up in app controller first - This is whats actually logging the user in and assigning the key/value pair to session hash
-            redirect to "/users/#{user.id}" #redirect to the users homepage
+            redirect to "/logs"
+            #redirect to "/users/#{user.id}" #redirect to the users homepage
             #redirect to "/users/#{@user.id}"
         else 
             flash[:message] = "Please enter a valid Username or Password"
