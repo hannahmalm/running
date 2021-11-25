8d85854c934e8da0214ada3ff1d78f24eede959d
Hannah Malm <hannahemalm616@gmail.com>
2021-11-24 19:13:32 -0500

    commented out post logs/

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -19,28 +19,35 @@ class LogsController < ApplicationController
         erb :'/log/new'
     end 
 
-    post '/logs' do 
-        not_logged_in_helper
-        if  
-            #params[:date] != "" && params[:distance] != "" && params[:pace] != "" && params[:user_id] != ""
+     post '/logs' do #DONE
+        if logged_in?
+            #@log = Log.create(:date => params[:date], :distance => params[:distance], :pace => params[:pace], :avg_heart_rate => params[:avg_heart_rate], :notes => params[:notes], :owner_id => params[:owner_id])
             @log = current_user.logs.build(:date => params[:date], :distance => params[:distance], :pace => params[:pace], :avg_heart_rate => params[:avg_heart_rate], :notes => params[:notes], :user_id => params[:user_id])
-            @log.save #@log.create does not work, do this workaround 
-            redirect to "/logs/#{@log.id}"
-        else 
-            #print a flash error stating that these fields are required
-            redirect to "/logs/new"
+            if @log.save
+                redirect to "/logs"
+                #redirect to "/logs/#{@log.id}"
+            else
+                redirect to "/logs/new"
+            end
+        else
+            redirect to '/login'
         end 
     end 
 
-    #show log 
-    # get '/logs/:id' do #DONE
-    #     if logged_in?
-    #         @log = Log.find_by_id(params[:id])
-    #         erb :'/log/show'
+    # post '/logs' do 
+    #     not_logged_in_helper
+    #     if  
+    #         #params[:date] != "" && params[:distance] != "" && params[:pace] != "" && params[:user_id] != ""
+    #         log = current_user.logs.build(:date => params[:date], :distance => params[:distance], :pace => params[:pace], :avg_heart_rate => params[:avg_heart_rate], :notes => params[:notes], :user_id => params[:user_id])
+    #         log.save #@log.create does not work, do this workaround 
+    #         redirect to "/logs/#{log.id}"
     #     else 
-    #         redirect to '/login'
+    #         #print a flash error stating that these fields are required
+    #         redirect to "/logs/new"
     #     end 
     # end 
+
+   
 
     get '/logs/:id' do 
         not_logged_in_helper
